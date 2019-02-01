b45a2db3e21b836a4800c8007c4a78ca38532668
Rawcloud <wu123473771@163.com>
2019-02-01 08:58:00 +0800

    抽出侧边栏，组件化

---
+++ src/components/SideBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="app-side-logo"
+      :width="isCollapse ? '60' : '60'"
+       @click.prevent="toggleSideBar"
+       height="60">
+       <img src="../../static/img/selflive.jpg" class="round_icon" :style="imgstyle"/>
+    </div>
+    <el-menu router :default-active="$route.path"
+                    class="el-menu-vertical-demo"
+                    @open="handleOpen"
+                    :collapse="isCollapse">
+        <SideBarItem
+          v-for="(item, idx) in routes"
+          :key="idx"
+          :subroute="item"
+          :fatherpath="fatherPath"
+          :barIdx="idx"></SideBarItem>
+    </el-menu>
+  </div>
+</template>
+<script>
+import SideBarItem from './SideBarItem'
+export default {
+  components: {
+    SideBarItem
+  },
+  data () {
+    return {
+      isCollapse: false,
+      classApse: 'app-side-collaped', // : 'app-side-expaned'
+      imgstyle: 'margin-left:80px;'
+    }
+  },
+  name: 'SideBar',
+  props: {
+    collapse: {
+      type: Boolean
+    },
+    routes: {
+      type: Array
+    }
+  },
+  computed: {
+    //   首次进入页面时展开当前页所属的菜单
+    defaultActive () {
+      return this.$route.path
+    },
+    // 直接获取路由器配置的'/'项
+    fatherPath () {
+      return this.$router.options.routes[1].path
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
---
+++ src/components/SideBarItem.vue
@@ -0,0 +1,72 @@
+<template>
+  <!-- 如果当前 subroute 柚子节点 -->
+  <el-submenu v-if="!subroute.hidden && subroute.children && subroute.children.length > 0"
+      :index="genPath(fatherpath, subroute.path)">
+    <!-- 创建分组菜单 -->
+    <template slot="title">
+        <i class="subroute.icon"></i>
+        <span slot="title">{{subroute.name}}</span>
+    </template>
+    <SideBarItem v-for="(submenu, subidx) in subroute.children"
+      :key="barIdx + '-' + subidx"
+      :subroute="submenu"
+      :fatherpath="genPath(fatherpath, subroute.path)"></SideBarItem>
+  </el-submenu>
+  <!-- 当前节点不含子节点且显示 -->
+  <el-menu-item v-else-if="!subroute.hidden"
+    style="font-weight:400;"
+    :index="genPath(fatherpath, subroute.path)">
+      {{subroute.name}}
+    </el-menu-item>
+    <el-menu-item v-else
+      style="font-weight:400;"
+      :index="genPath(fatherpath, subroute.path)">
+        {{subroute.name}}
+      </el-menu-item>
+</template>
+<script>
+export default {
+  name: 'SideBarItem',
+  props: {
+    subroute: {
+      type: Object
+    },
+    barIdx: {
+      type: [String, Number]
+    },
+    fatherpath: {
+      type: String
+    }
+  },
+  computed: {
+    defaultActive: function () {
+      return this.$route.path
+    }
+  },
+  methods: {
+    genPath: function () {
+      let arr = [...arguments]
+      let path = arr.map(v => {
+        while (v[0] === '/') {
+          v = v.substring(1)
+        }
+        while (v[-1] === '/') {
+          v = v.substring(0, v.length)
+        }
+        return v
+      }).join('/')
+      path = path[0] === '/' ? path : '/' + path
+      return path
+    },
+    handleOpen: function (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose: function (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  },
+  mounted () {
+    console.log('sidebar routes:', this.routes)
+  }
+}
+</script>
